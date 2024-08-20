920d46a2580ce9625d797559efa7c6078575e6ea
Garic152 <erictheiler15@gmail.com>
2024-08-20 12:32:18 +0200

    feat: reworked the general ratatui layout

--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,98 @@
-use chrono::Local;
-use rusqlite::{params, Connection, Result};
-use std::io;
+use std::{self, io};
 
 use ratatui::{
-    buffer::Buffer,
-    crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
-    layout::{Alignment, Rect},
-    style::Stylize,
-    symbols::border,
-    text::{Line, Text},
-    widgets::{
-        block::{Position, Title},
-        Block, Paragraph, Widget,
+    backend::{Backend, CrosstermBackend},
+    crossterm::{
+        event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
+        execute,
+        terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+        ExecutableCommand,
     },
-    Frame,
+    widgets::{Block, Paragraph},
+    Frame, Terminal,
 };
 
-mod tui;
+enum AppState {
+    StartMenu,
+    NewObservation,
+    AddObservation,
+}
 
-#[derive(Debug, Default)]
 pub struct App {
-    counter: u8,
-    exit: bool,
+    state: AppState,
+    quit: bool,
 }
 
 impl App {
-    pub fn run(&mut self, terminal: &mut tui::Tui) -> io::Result<()> {
-        while !self.exit {
-            terminal.draw(|frame| self.render_frame(frame))?;
-            self.handle_events()?;
+    pub fn default() -> Self {
+        App {
+            state: AppState::StartMenu,
+            quit: false,
         }
-        Ok(())
-    }
-
-    fn render_frame(&self, frame: &mut Frame) {
-        frame.render_widget(self, frame.area());
-    }
-
-    fn handle_key_event(&mut self, key_event: KeyEvent) {
-        match key_event.code {
-            KeyCode::Char('q') => self.exit(),
-            KeyCode::Char('1') => todo!(),
-            _ => {}
-        }
-    }
-
-    fn handle_events(&mut self) -> io::Result<()> {
-        match event::read()? {
-            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-                self.handle_key_event(key_event)
-            }
-            _ => {}
-        };
-        Ok(())
-    }
-
-    fn exit(&mut self) {
-        self.exit = true;
     }
 }
 
-impl Widget for &App {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        let title = Title::from(" ObSt observation tracking tool ".bold());
-        let instructions = Title::from(Line::from(vec![
-            " New Observation ".into(),
-            "<1>".blue().bold(),
-            " Add Observation ".into(),
-            "<2>".blue().bold(),
-            // compare buffers and display the differences in a more readable way
-            " Quit ".into(),
-            "<Q> ".blue().bold(),
-        ]));
-        Block::bordered()
-            .title(title.alignment(Alignment::Center))
-            .title(
-                instructions
-                    .alignment(Alignment::Center)
-                    .position(Position::Bottom),
-            )
-            .border_set(border::THICK)
-            .render(area, buf);
+fn handle_events() -> io::Result<bool> {
+    if event::poll(std::time::Duration::from_millis(50))? {
+        if let Event::Key(key) = event::read()? {
+            if key.kind == event::KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                return Ok(true);
+            }
+        }
+    }
+    Ok(false)
+}
+
+fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> io::Result<()> {
+    loop {
+        if app.quit {
+            return Ok(());
+        }
+        terminal.draw(|f| ui(f, app))?;
+
+        if let Event::Key(key) = event::read()? {
+            match app.state {
+                AppState::StartMenu => match key.code {
+                    KeyCode::Char('q') => app.quit = true,
+                    KeyCode::Char('1') => {
+                        app.state = AppState::NewObservation;
+                    }
+                    KeyCode::Char('2') => {
+                        app.state = AppState::AddObservation;
+                    }
+                    _ => {}
+                },
+                AppState::NewObservation => match key.code {
+                    KeyCode::Char('q') => app.state = AppState::StartMenu,
+                    _ => {}
+                },
+                AppState::AddObservation => match key.code {
+                    KeyCode::Char('q') => app.state = AppState::StartMenu,
+                    _ => {}
+                },
+            }
+        }
     }
 }
 
 fn main() -> io::Result<()> {
-    let mut terminal = tui::init()?;
-    let app_result = App::default().run(&mut terminal);
-    tui::restore()?;
-    app_result
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    let mut app = App::default();
+    let res = run_app(&mut terminal, &mut app)?;
+
+    disable_raw_mode()?;
+    execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
+    Ok(())
+}
+
+fn ui(frame: &mut Frame, app: &mut App) {
+    frame.render_widget(
+        Paragraph::new("Hello World!").block(Block::bordered().title("Greeting")),
+        frame.area(),
+    );
 }
