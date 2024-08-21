1de049f7af782b6950837de5ff224b58750c8a65
Garic152 <erictheiler15@gmail.com>
2024-08-21 12:50:46 +0200

    feat: Started layout for new observation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{self, io};
+use std::{self, io, str};
 
 use ratatui::{
     backend::{Backend, CrosstermBackend},
@@ -8,7 +8,7 @@ use ratatui::{
         terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
         ExecutableCommand,
     },
-    layout::Alignment,
+    prelude::*,
     style::Stylize,
     symbols::border,
     text::{Line, Text},
@@ -25,17 +25,58 @@ enum AppState {
     AddObservation,
 }
 
+enum NewObservationSteps {
+    Name,
+    Date,
+    Amount,
+    Declaration,
+    Confirmation,
+}
+
 pub struct App {
     state: AppState,
+    selected_menu: i8,
+    new_observation_step: NewObservationSteps,
+    confirmations_left: i8,
+    observation: Observation,
+    input: String,
+    character_index: usize,
+    data: Vec<String>,
     quit: bool,
+}
+
+struct Observation {
+    name: String,
+    parameters: Vec<Vec<String>>,
 }
 
 impl App {
     pub fn default() -> Self {
         App {
             state: AppState::StartMenu,
+            selected_menu: 0,
+            new_observation_step: NewObservationSteps::Name,
+            confirmations_left: 0,
+            observation: Observation {
+                name: ("".to_string()),
+                parameters: (Vec::new()),
+            },
+            input: String::new(),
+            character_index: 0,
+            data: Vec::new(),
             quit: false,
         }
+    }
+    pub fn new_observation_process_confirmation(&mut self) {
+        match self.new_observation_step {
+            NewObservationSteps::Date => {}
+            _ => {}
+        }
+    }
+    pub fn new_observation_add_date(&mut self) {
+        self.observation
+            .parameters
+            .push(vec![String::from("Date"), String::from("DATE")]);
     }
 }
 
@@ -52,6 +93,7 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> io::Result<
                     KeyCode::Char('q') => app.quit = true,
                     KeyCode::Char('1') => {
                         app.state = AppState::NewObservation;
+                        app.confirmations_left = 1;
                     }
                     KeyCode::Char('2') => {
                         app.state = AppState::AddObservation;
@@ -60,7 +102,17 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> io::Result<
                 },
                 AppState::NewObservation => match key.code {
                     KeyCode::Char('q') => app.state = AppState::StartMenu,
-                    _ => {}
+
+                    _ => match app.new_observation_step {
+                        NewObservationSteps::Date => match key.code {
+                            KeyCode::Char('y') => app.new_observation_add_date(),
+                            KeyCode::Char('n') => {
+                                app.new_observation_step = NewObservationSteps::Amount
+                            }
+                            _ => {}
+                        },
+                        _ => {}
+                    },
                 },
                 AppState::AddObservation => match key.code {
                     KeyCode::Char('q') => app.state = AppState::StartMenu,
@@ -92,23 +144,58 @@ fn ui(frame: &mut Frame, app: &mut App) {
             let title = Title::from(" ObSt Main Menu ".bold());
 
             frame.render_widget(
-                Paragraph::new("\nWelcome to the ObSt Observation tracking tool!\n")
-                    .centered()
-                    .block(Block::bordered().title(title.alignment(Alignment::Center)))
-                    .alignment(Alignment::Center),
+                Paragraph::new(
+                    "\nWelcome to the ObSt Observation tracking tool!\n
+                    \nPress one of the following keys to enter the corresponding submenus:
+                    \n1) Create a new observation
+                    \n2) Add to an existing observation
+                    \nq) Quit the TUI",
+                )
+                .centered()
+                .block(Block::bordered().title(title.alignment(Alignment::Center)))
+                .alignment(Alignment::Center),
                 frame.area(),
             );
         }
         AppState::NewObservation => {
             let title = Title::from(" New Observation ".bold());
 
+            let outer_block = Block::bordered().title(title);
+            let message = match app.new_observation_step {
+                NewObservationSteps::Name => Paragraph::new("\nYou will now be able to add a new observation to the database.\nStarting off, give your observation a name.").centered(),
+                NewObservationSteps::Date => Paragraph::new("\nDo you intend to track the date for your observation?").centered(),
+                NewObservationSteps::Amount => Paragraph::new("\nHow many correlated variables do you want to observe?").centered(),
+                NewObservationSteps::Declaration => Paragraph::new("\nPlease now declare the variables you want to observe.\nIn order to do that, fill in the highlighted box and press enter to confirm.").centered(),
+                NewObservationSteps::Confirmation => Paragraph::new("\nTODO!")
+            };
+
+            let inner_block = Block::bordered()
+                .title("Input")
+                .title_alignment(Alignment::Center);
+            let input = match app.new_observation_step {
+                NewObservationSteps::Name => {
+                    Paragraph::new(app.input.as_str()).style(Style::default())
+                }
+                _ => Paragraph::new("\nTODO!"),
+            };
+
+            let inner_area = outer_block.inner(frame.area());
+
+            let chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .margin(1)
+                .constraints([Constraint::Length(5), Constraint::Length(3)].as_ref())
+                .split(inner_area);
+
             frame.render_widget(
-                Paragraph::new("\nTest")
+                message
                     .centered()
-                    .block(Block::bordered().title(title.alignment(Alignment::Center)))
+                    .block(Block::default())
                     .alignment(Alignment::Center),
-                frame.area(),
+                chunks[0],
             );
+            frame.render_widget(input.block(inner_block), chunks[1]);
+            frame.render_widget(outer_block, frame.area());
         }
         AppState::AddObservation => {
             let title = Title::from(" Add Observation ".bold());
